49c338f1bdb9c905a2ebbb60429fd0e479612aee
Dice-K39 <daisukehashimoto39@gmail.com>
2020-09-23 22:36:15 -0400

    Creating the Programmer MVC.

--- app/controllers/programmers_controller.rb
+++ app/controllers/programmers_controller.rb
@@ -1,2 +1,37 @@
 class ProgrammersController < ApplicationController
+    def index
+        @programmers = Programmer.all
+    end
+
+    def new
+        @programmer = Programmer.new
+    end
+
+    def create
+        programmer = Programmer.new(programmer_params)
+
+        if programmer.save
+            redirect_to programmer_path(programmer)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @programmer = find_programmer
+    end
+
+    def update
+        @programmer = find_programmer
+    end
+
+    private
+
+    def programmer_params
+        params.require(:programmer).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email, :phone_number)
+    end
+
+    def find_programmer
+        Programmer.find_by_id(params[:id])
+    end
 end
--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,4 +1,13 @@
 class Programmer < ApplicationRecord
+    has_secure_password
+    
     has_many :assignments
     has_many :projects, through: :assignments
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :phone_number, presence: true, length: {10}
 end
