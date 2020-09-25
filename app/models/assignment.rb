81d9752ef25bfbab2bca3f75ef87c4f76b87a895
Dice-K39 <daisukehashimoto39@gmail.com>
2020-09-25 18:32:07 -0400

    Light refactoring.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,6 @@
 class Assignment < ApplicationRecord
     has_many :comments
     
-    belongs_to :programmer
-    belongs_to :project
+    belongs_to :programmer, optional: true
+    belongs_to :project, optional: true
 end
--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -4,10 +4,7 @@ class Programmer < ApplicationRecord
     has_many :assignments
     has_many :projects, through: :assignments
 
-    validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true, uniqueness: true
+    validates :username, :email, presence: true, uniqueness: true
+    validates :password, :first_name, :last_name, presence: true
     validates :phone_number, presence: true, length: {is: 10}
 end
