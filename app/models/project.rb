46cf6f0284f14f018ac275ec022260daf96abb3e
Dice-K39 <daisukehashimoto39@gmail.com>
2020-09-24 23:50:05 -0400

    Started constructing the Project model.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
     has_many :assignments
     has_many :programmers, through: :assignments
+
+    enum status: ['PLANNING', 'IN_PROGRESS', 'COMPLETED', 'OVERDUE']
+
+    validates :name, :due_date, :status, presence: true
+    validates :description, length: {minimum: 20}
 end
