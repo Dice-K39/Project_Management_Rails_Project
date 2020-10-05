48842355d101f54a548e1d2587ee6fb5964fc046
Dice-K39 <daisukehashimoto39@gmail.com>
2020-10-04 23:31:43 -0400

    Cleaning up code.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
     before_action :if_not_logged_in_redirect_to_login
+    before_action :authorize, only: [:edit, :update, :destroy]
 
     def index
-        @assignment = Assignment.find_by_id(params[:assignment_id])
+        find_assignment
         @comments = @assignment.comments
         # @comments = Assignment.find_by_id(params[:assignment_id]).comments
     end
@@ -10,12 +11,12 @@ class CommentsController < ApplicationController
     def new
         does_assignment_exist?(params[:assignment_id])
 
-        @assignment = Assignment.find_by_id(params[:assignment_id])
+        find_assignment
         @comment = @assignment.comments.new
     end
 
     def create
-        @assignment = Assignment.find_by_id(params[:assignment_id])
+        find_assignment
         comment = @assignment.comments.new(comment_params)
 
         comment.programmer_id = current_programmer.id
@@ -30,21 +31,17 @@ class CommentsController < ApplicationController
     end
 
     def show
-        @assignment = Assignment.find_by_id(params[:assignment_id])
+        find_assignment
         @comment = @assignment.comments.find_by_id(params[:id])
     end
 
     def edit
-        redirect_if_not_current_programmer_or_project_manager(session[:programmer_id])
-
-        @assignment = Assignment.find_by_id(params[:assignment_id])
-        @comment = Comment.find_by_id(params[:id])
+        find_assignment
+        find_comment
     end
 
     def update
-        redirect_if_not_current_programmer_or_project_manager(session[:programmer_id])
-
-        @comment = Comment.find_by_id(params[:id])
+        find_comment
 
         if @comment.update(comment_params)
             redirect_to assignment_comments_path(@comment.assignment_id)
@@ -53,15 +50,13 @@ class CommentsController < ApplicationController
         end
     end
 
-    def destroy
-        redirect_if_not_current_programmer_or_project_manager(session[:programmer_id])
+    def destroy        
+        find_comment
         
-        comment = Comment.find_by_id(params[:id])
-
-        if comment.delete
-            redirect_to assignment_comments_path(comment.assignment_id)
+        if @comment.delete
+            redirect_to assignment_comments_path(@comment.assignment_id)
         else
-            redirect_to assignment_comments_path(comment)
+            redirect_to assignment_comments_path(@comment)
         end
     end
 
@@ -69,5 +64,17 @@ class CommentsController < ApplicationController
 
     def comment_params
         params.require(:comment).permit(:title, :content, :assignment_id, :programmer_id)
+    end
+
+    def find_assignment
+        @assignment = Assignment.find_by_id(params[:assignment_id])
+    end
+
+    def find_comment
+        @comment = Comment.find_by_id(params[:id])
+    end
+
+    def authorize
+        redirect_if_not_current_programmer_or_project_manager(session[:programmer_id])
     end
 end
